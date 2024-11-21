ddf6cd4842ae71d4536b4a4ab5cf652bff90c361
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-21 21:00:06 +0700

    memanggil pengelolahalaman di MainActivity

--- app/src/main/java/com/example/project6/MainActivity.kt
+++ app/src/main/java/com/example/project6/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             Project6Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    PengelolaHalaman()
                 }
             }
         }
