208c836dfc9c65dbb28b6796d3430b5f1a3cfb59
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-21 21:02:39 +0700

    menambah variabel nim

--- app/src/main/java/com/example/project6/model/DataMahasiswa.kt
+++ app/src/main/java/com/example/project6/model/DataMahasiswa.kt
@@ -2,6 +2,7 @@ package com.example.project6.model
 
 data class DataMahasiswa(
     val nama : String = "",
+    val nim : String = "",
     val gender : String = "",
     val alamat : String = "",
 )
